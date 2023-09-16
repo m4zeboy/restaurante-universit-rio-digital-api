2cca72652a0d5e2c3368dad32d363986f548eac2
m4zeboy <moises775azevedo@gmail.com>
2023-09-16 11:44:29 -0300

    Estrutura do projeto

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Restaurante Universitário Digial - API
+
+## Estrutura do projeto
+
+- [x] Iniciar projeto em node
+- [x] Scripts
+- [x] Versões exatas do npm
+- [x] Carregar varíaveis de ambiente
+- [x] Criar aliases de importação
---
+++ src/env/index.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod'
+import 'dotenv/config'
+
+const envSchema = z.object({
+  NODE_ENV: z
+    .enum(['development', 'test', 'production'])
+    .default('development'),
+  PORT: z.coerce.number().default(3333),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (!_env.success) {
+  console.log('❌ Invalid environment variables.')
+  throw new Error('Invalid environment variables')
+}
+
+export const env = _env.data
